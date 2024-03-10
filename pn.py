43f1eed6b2aab9d2df51492ac9866935abfe2ca3
Dedi Rohadi <155202658+wapbaru@users.noreply.github.com>
2024-03-10 18:46:51 +0700

    Update pn.py

--- pn.py
+++ pn.py
@@ -42,7 +42,7 @@ try:
 	print("\033[1;33m> \033[1;32mLokasi\033[0m: ",x)
 	print("\033[1;33m> \033[1;32mSingkatan\033[0m: ",phonenumbers.region_code_for_number(pn))
 	print("\033[1;33m> \033[1;32mNomor terlalu panjang\033[0m: ",phonenumbers.truncate_too_long_number(pn))
-	print("\033[1;33m> \033[1;32mOrder Smm no #1 Hanya di\033[0m: ",https://Wapbaru.com)
+	print("\033[1;33m> \033[1;32mOrder Smm no #1 Hanya di https://Wapbaru.com\033[0m")
 
 except KeyboardInterrupt:
 	print("\033[1;41mTerimakasih banyak!\033[0m")
